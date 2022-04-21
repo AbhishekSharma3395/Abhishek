d0e7f12ee22f343f76265f7f089f5301eb6660d2
AbhishekSharma3395 <104124339+AbhishekSharma3395@users.noreply.github.com>
2022-04-21 11:57:45 +0530

    Add files via upload

---
+++ Insert.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Insert.aspx.cs" Inherits="insert.Insert" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+            <asp:Label ID="Label1" runat="server" Text="Enter Name"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Label ID="Label2" runat="server" Text="Enter Pass"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <asp:Label ID="Label3" runat="server" Text="EId"></asp:Label>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <asp:Label ID="Label4" runat="server" Text="Enter ID"></asp:Label>
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            
+        </div>
+        <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Delete" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Update" />
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click"  Text="Search" />
+         <asp:Button ID="Button5" runat="server" OnClick="Button5_Click"  Text="Show" />
+        <asp:GridView ID="GridView1" runat="server" Width="547px">
+        </asp:GridView>
+
+
+    </form>
+</body>
+</html>
---
+++ Insert.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace insert
+{
+    public partial class Insert : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+    
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-10P465B;Initial Catalog=dd;Integrated Security=True");
+            con.Open();
+            string query = "insert into Table_1 (id,name,pass,eid) values (@id,@name,@pass,@eid)";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@name", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@pass", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@eid", TextBox3.Text);
+            cmd.Parameters.AddWithValue("@id", TextBox4.Text);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Write("<script>alert('Data inserted successfully')</script>");
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-10P465B;Initial Catalog=dd;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete Table_1 where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", TextBox4.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Write("<script>alert('Data Deleted successfully')</script>");
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-10P465B;Initial Catalog=dd;Integrated Security=True");
+            con.Open();
+            string query = "update Table_1 set name=@name,pass=@pass,eid=@eid where id=@id";
+            SqlCommand cmd = new SqlCommand(query,con);
+            cmd.Parameters.AddWithValue("@name", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@pass", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@eid", TextBox3.Text);
+            cmd.Parameters.AddWithValue("@id", TextBox4.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            Response.Write("<script>alert('Data Update successfully')</script>");
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-10P465B;Initial Catalog=dd;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from Table_1 where id=@id", con);
+            cmd.Parameters.AddWithValue("id",(TextBox4.Text));
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            con.Close();
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-10P465B;Initial Catalog=dd;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from Table_1", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            con.Close();
+        }
+
+    }
+}
