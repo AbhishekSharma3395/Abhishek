256640c768cb0d197a8f09ce0940b8b54a5a6f9a
AbhishekSharma3395 <104124339+AbhishekSharma3395@users.noreply.github.com>
2022-05-05 23:18:31 +0530

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+namespace Label
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected System.Web.UI.HtmlControls.HtmlInputFile File1;
+        protected System.Web.UI.HtmlControls.HtmlInputButton Submit1;
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if ((FileUpload1.PostedFile != null) && (FileUpload1.PostedFile.ContentLength > 0))
+            {
+                string fn = System.IO.Path.GetFileName(FileUpload1.PostedFile.FileName);
+                string SaveLocation = Server.MapPath("upload") + "\\" + fn;
+                try
+                {
+                    FileUpload1.PostedFile.SaveAs(SaveLocation);
+                    FileUploadStatus.Text = "The file has been uploaded.";
+                }
+                catch (Exception ex)
+                {
+                    FileUploadStatus.Text = "Error: " + ex.Message;
+                }
+            }
+            else
+            {
+                FileUploadStatus.Text = "Please select a file to upload.";
+            }
+        }
+   
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+        {
+            string filePath = "C:\\Users\\DELL\\source\\repos\\Label\\Label\\abc.txt";
+            FileInfo file = new FileInfo(filePath);
+            if (file.Exists)
+            {
+                // Clear Rsponse reference  
+                Response.Clear();
+                // Add header by specifying file name  
+                Response.AddHeader("Content-Disposition", "attachment; filename=" + file.Name);
+                // Add header for content length  
+                Response.AddHeader("Content-Length", file.Length.ToString());
+                // Specify content type  
+                Response.ContentType = "text/plain";
+                // Clearing flush  
+                Response.Flush();
+                // Transimiting file  
+                Response.TransmitFile(file.FullName);
+                Response.End();
+            }
+
+            else
+            {
+                Label12.Text = "Requested file is not available to download";
+            }
+
+
+            Label6.Text = TextBox1.Text;
+            Label11.Text = TextBox2.Text;
+            Label8.Text = TextBox3.Text;
+            Label9.Text = TextBox4.Text;
+            Label10.Text = TextBox5.Text;
+        }
+
+    }
+}
